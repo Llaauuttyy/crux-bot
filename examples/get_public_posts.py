c5d5896ea1919641c37066a284e760524a344cea
leonelcha18 <leonel.a.cha@gmail.com>
2020-12-22 22:32:57 -0300

    Se preconfigura ejemplo de conexión
    Se setean algunas variables, con datos originados de la cuenta de
    crux-bot de "Facebook for Developers".
    
    Se comentan algunos puntos importantes, para entender la lógica
    implicada en la conexión y comunicación.

--- examples/get_public_posts.py
+++ examples/get_public_posts.py
@@ -8,19 +8,27 @@ from pyfacebook import Api
 
 # Use version 5+, Call API need app secret proof. So need provide your app secret.
 # If not have, you can just use version 4.0.
-APP_ID = "Your APP ID"
-APP_SECRET = "Your APP SECRET"
+# El valor de estas variables, se obtienen de la cuenta en Facebook for Developers.
+APP_ID = "2522931991341291"
+APP_SECRET = "9552895069b4d3c2950320c0f06354ff"
 
+# Se deberá omitir setear ésta variable, por cuestiones de seguridad
 ACCESS_TOKEN = "Your Access Token"
 
 
 def get_posts(page_username):
+    # Se crea un objeto Api para la conexión, a partir del contructor, al cual se le
+    # pasa por parámetros, las constantes anteriormente definidas.
     api = Api(
         app_id = APP_ID,
         app_secret = APP_SECRET,
         long_term_token = ACCESS_TOKEN,
     )
 
+    # Se llama a un método del objeto Api, el cual nos devuelve los posteos hechos por
+    # el usuario, en su muro.
+    # Hay algunos filtros que se pueden pasar por parámetro, para manipular que
+    # información se desea obtener.
     data = api.get_page_posts(
         page_id = page_username,
         since_time = "2020-05-01",
@@ -33,7 +41,12 @@ def get_posts(page_username):
 
 
 def processor():
-    page_username = "WHO"
+    # En este campo, se debe setear el id de usuario. El mismo se obtiene desde la cuenta
+    # normal de Facebook en "Configuración/Integraciones comerciales", y en la aplicación
+    # que se creó, dar click en "Ver y editar".
+    # También se puede obtener desde Facebook for Developers, haciendo una consulta desde
+    # el "Explorador de la API Graph"
+    page_username = "103684888301061"
     data = get_posts(page_username)
     
     with open("wto_posts.json", 'w') as f:
