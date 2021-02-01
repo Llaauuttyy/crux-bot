217921bbfab9d52a02f0106f5cd3248e5b204cf1
Lautaro D'abbraccio <ldabbraccio@fi.uba.ar>
2021-02-01 00:16:23 -0300

    Facebook tests are uploaded
    Current tests are used in order to check
    new funtionalities added for expanding
    pyFacebook API.

---
+++ tests/facebook/test_post_photo.py
@@ -0,0 +1,36 @@
+import unittest
+import json
+import sys
+sys.path.append("ALTERNATIVE PATH")
+
+import examples.extended_api as extended_api
+
+
+class ConversationMessagesTest(unittest.TestCase):
+
+    def setUp(self):
+        self.page_id = "102579945106245"
+        self.page_access_token = "PAGE_ACCESS_TOKEN"
+
+        self.api = extended_api.ExtApi(long_term_token="long-term-token")
+
+    def testPostPhoto(self):
+
+        self.api.post_photo(
+            page_id=self.page_id,
+            access_token=self.page_access_token,
+            files=open("images.jpg", 'rb'),
+            return_json=True
+        )
+
+        data = []
+
+        with open("data\\facebook\\fb_post_photo.json") as file:
+            data = json.load(file)
+
+        self.assertTrue("id" in data)
+        self.assertTrue("post_id" in data)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/facebook/test_put_like.py
@@ -0,0 +1,34 @@
+import unittest
+import json
+import sys
+sys.path.append("ALTERNATIVE PATH")
+
+import examples.extended_api as extended_api
+
+
+class ConversationMessagesTest(unittest.TestCase):
+
+    def setUp(self):
+        self.object_id = "102579945106245_117019143662325"
+        self.page_access_token = "PAGE_ACCESS_TOKEN"
+
+        self.api = extended_api.ExtApi(long_term_token="long-term-token")
+
+    def testPutLike(self):
+
+        self.api.put_like(
+            object_id=self.object_id,
+            access_token=self.page_access_token,
+            return_json=True
+        )
+
+        data = []
+
+        with open("data\\facebook\\fb_put_like.json") as file:
+            data = json.load(file)
+
+        self.assertTrue("success" in data)
+
+
+if __name__ == "__main__":
+    unittest.main()
