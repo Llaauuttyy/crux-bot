77f733011268111da150d9a910e500ddf4610668
Agustin Sanchez <asanchezd@fi.uba.ar>
2021-02-05 18:57:49 +0100

    The language was change

---
+++ menu.py
@@ -0,0 +1,4 @@
+def display_menu_fb():
+    print("1.Dar like a un posteo\n2.Leer posteo\n3.Subir posteo o foto\n4.Actualizar perfil\n5.Listar amigos\n6.Listar seguidores\n7.Seguir a un usuario\n8.Enviar un mensaje a un usuario")
+def display_menu_ig():
+    print("1.Buscar usuario\n2.Listar seguidores\n3.Modificar publicacion\n4.Publicar foto\n5.Mostrar posteo")
