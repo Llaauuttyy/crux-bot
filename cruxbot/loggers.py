dde409f2457aa7a1517caf93e6d37f1c008c1048
Lautaro D'abbraccio <ldabbraccio@fi.uba.ar>
2021-02-06 23:43:46 -0300

    Loggers logic has been changed

--- cruxbot/loggers.py
+++ cruxbot/loggers.py
@@ -1,62 +1,76 @@
 import logging
 
 
-# DEBUG logger is created
-
-debug_logger = logging.getLogger("debug")
-debug_logger.setLevel(logging.DEBUG)
-
-debug_formatter = logging.Formatter(
-    "| %(asctime)s | Line %(lineno)d | %(levelname)s | (Module: %(module)s): (Function: %(funcName)s): %(message)s.",
-    "%d/%m/%Y - %H:%M:%S"
-)
-
-debug_handler = logging.FileHandler("logs\\debug.log", "a")
-debug_handler.setFormatter(debug_formatter)
-
-debug_logger.addHandler(debug_handler)
-
-debug_logger.debug("Debug message")
-debug_logger.debug("Debug message")
-debug_logger.debug("Debug message")
+# ------------------------------------------------- #
+# ------------ DEBUG LOGGER IS CREATED ------------ #
+# ------------------------------------------------- #
 
 
-# CHAT logger is created
+def debug_logger_creation():
 
-chat_logger = logging.getLogger("chat")
-chat_logger.setLevel(logging.INFO)
+    debug_logger = logging.getLogger("debug")
+    debug_logger.setLevel(logging.DEBUG)
 
-chat_formatter = logging.Formatter(
-    '''%(asctime)s, %(message)s.''',
-    "%d/%m/%Y, %H:%M:%S"
-)
+    debug_formatter = logging.Formatter(
+        "| %(asctime)s | Line %(lineno)d | %(levelname)s | (Module: %(module)s): (Function: %(funcName)s): %(message)s",
+        "%d/%m/%Y - %H:%M:%S"
+    )
 
-chat_handler = logging.FileHandler("logs\\chat.log", "a")
-chat_handler.setFormatter(chat_formatter)
+    debug_handler = logging.FileHandler("logs\\debug.log", "a")
+    debug_handler.setFormatter(debug_formatter)
 
-chat_logger.addHandler(chat_handler)
+    debug_logger.addHandler(debug_handler)
 
-chat_logger.info("Crux, Hiiiiiiiiiii")
-chat_logger.info("Crux, Hiiiiiiiiiii")
-chat_logger.info("Crux, Hiiiiiiiiiii")
+    return debug_logger
 
 
-# ERROR logger is created
-
-error_logger = logging.getLogger("error")
-error_logger.setLevel(logging.ERROR)
-
-error_formatter = logging.Formatter(
-    "| %(asctime)s | Line %(lineno)d | %(levelname)s | (Module: %(module)s): (Function: %(funcName)s): %(message)s.",
-    "%d/%m/%Y - %H:%M:%S"
-)
-
-error_handler = logging.FileHandler("logs\\error.log", "a")
-error_handler.setFormatter(error_formatter)
-
-error_logger.addHandler(error_handler)
+# ------------------------------------------------ #
+# ------------ CHAT LOGGER IS CREATED ------------ #
+# ------------------------------------------------ #
 
 
-error_logger.error("Error message")
-error_logger.error("Error message")
-error_logger.error("Error message")
+def chat_logger_creation():
+
+    chat_logger = logging.getLogger("chat")
+    chat_logger.setLevel(logging.INFO)
+
+    chat_formatter = logging.Formatter(
+        '''%(asctime)s, %(message)s.''',
+        "%d/%m/%Y, %H:%M:%S"
+    )
+
+    chat_handler = logging.FileHandler("logs\\chat.log", "a")
+    chat_handler.setFormatter(chat_formatter)
+
+    chat_logger.addHandler(chat_handler)
+
+    return chat_logger
+
+
+# ------------------------------------------------- #
+# ------------ ERROR LOGGER IS CREATED ------------ #
+# ------------------------------------------------- #
+
+
+def error_logger_creation():
+
+    error_logger = logging.getLogger("error")
+    error_logger.setLevel(logging.ERROR)
+
+    error_formatter = logging.Formatter(
+        "| %(asctime)s | Line %(lineno)d | %(levelname)s | (Module: %(module)s): (Function: %(funcName)s): %(message)s",
+        "%d/%m/%Y - %H:%M:%S"
+    )
+
+    error_handler = logging.FileHandler("logs\\error.log", "a")
+    error_handler.setFormatter(error_formatter)
+
+    error_logger.addHandler(error_handler)
+
+    return error_logger
+
+
+# Loggers
+debug_logger = debug_logger_creation()
+chat_logger = chat_logger_creation()
+error_logger = error_logger_creation()
