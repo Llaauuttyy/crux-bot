c0f084115549bdff22be117e5857b03f17403541
Leonel Chaves <lchaves@fi.uba.ar>
2021-02-09 17:59:19 -0300

    Reestructuración de archivo principal
    Se eliminan try except innecesarios.
    
    Se eliminan llamados a loggers
    
    Se reestructura lógica de main()

--- cruxbot/cruxbot.py
+++ cruxbot/cruxbot.py
@@ -1,12 +1,13 @@
 import sys
-sys.path.append("C:/Users/Asus/Desktop/Repositorios/crux-bot")
+sys.path.append("C:/Users/Leonel/Documents/crux-bot")
 
-import loggers as log
 import facebook_actions as fb
+import instagram_actions as ig
+from loggers import chat_logger
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-from pyfacebook import Api
+from pyfacebook import Api, IgProApi
 
 
 # Cruxbot's app ID
@@ -16,7 +17,7 @@ APP_SECRET = "9552895069b4d3c2950320c0f06354ff"
 
 USER_ACCESS_TOKEN = ""
 
-PAGE_ACCESS_TOKEN = ""
+PAGE_ACCESS_TOKEN = "EAAj2lZBEi6OsBAKUmb5ZCgYiHrrJzTnfBmUwItETHsrAZC96gnNL0jdhBG31NpFhhZCHTLgmEdCJulQ6S491ZC3QjYiOh9UJjPAFrWFYCzDfZBEwLOEaH5oNwmllruC47SM7PlwycoDgKFLagXfTLHzmZBixziVTdmYX5nnNMqS4AZDZD"
 
 # Cruxbot's user ID
 USER_ID = "103684888301061"
@@ -48,7 +49,7 @@ def posts_printing(posts_info_list  # type: dict
 
             if key == "message":
                 print(f"Post {posts + 1}: {posts_info_list[posts]['message']}\n")
-                log.chat_logger.info("Crux, Post {post_number}: {post_message}".format(
+                chat_logger.info("Crux, Post {post_number}: {post_message}".format(
                     post_number=posts + 1,
                     post_message=posts_info_list[posts]['message']
                 ))
@@ -58,7 +59,7 @@ def posts_printing(posts_info_list  # type: dict
 
             elif key == "picture":
                 print(f"Post {posts + 1}: This post is a photo.")
-                log.chat_logger.info("Crux, Post {post_number}: {post_message}".format(
+                chat_logger.info("Crux, Post {post_number}: {post_message}".format(
                     post_number=posts + 1,
                     post_message="This post is a photo."
                 ))
@@ -77,8 +78,8 @@ def posts_order(posts_info_list,  # type: dict
     # Then seaches for the post id and returns it.
 
     post_index = int(post_number) - 1  # Beacuse we're using a list.
-    print("[Crux]: Obteniendo datos...")
-    log.chat_logger.info("Crux, Obteniendo datos...")
+
+
 
     post_id = posts_info_list[post_index]["id"]
 
@@ -121,16 +122,9 @@ def bot_creation():
     # POST: Creates the bot object right
     # after deletes all the previous storage.
     # Finally, returns the bot object.
-    try:
-        chatbot = ChatBot("Crux")
-        chatbot.storage.drop()
+    chatbot = ChatBot("Crux")
 
-    except:
-        log.error_logger.error("bot_creation failed")
-        raise Exception("Something went wrong while creating bot.")
-
-    else:
-        return chatbot
+    return chatbot
 
 
 def bot_training(bot  # type: ChatBot
@@ -141,38 +135,24 @@ def bot_training(bot  # type: ChatBot
     # POST: Uses it in order to create
     # the trainer object and allows us
     # to train our bot with a string list.
+    datos = []
+    bot.storage.drop()
+    trainer = ListTrainer(bot)
 
-    try:
-        trainer = ListTrainer(bot)
+    with open("data/entrenador.txt", "r", encoding = "utf-8") as f:
+        datos = f.read().splitlines()
 
-        trainer.train([
-            "Hola, soy Juan",
-            "Hola, bienvenido. Mi nombre es Crux",
-            "Quiero ver mis publicaciones",
-            "Perfecto, Juan. Aqui estan tus publicaciones:",
-            "Quiero likear una publicacion",
-            "Excelente, Juan. Vamos a likear la publicacion",
-            "Quiero comentar la ultima publicacion",
-            "Maravilloso, Juan. Aqui vamos"
-        ])
-
-    except:
-        log.error_logger.error("bot_training failed")
-        raise Exception("Something went wrong while training bot")
+    trainer.train(datos)
 
 
-def bot_greetings():
-    greeting_message = "Bienvenido. Me llamo Crux. ¿Cuál es tu nombre? (Sólo escribe tu nombre)"
-    log.chat_logger.info(f"Crux, {greeting_message}")
+def bot_greetings(bot  # type: ChatBot
+                 ):
 
-    user_name = input(f"[Crux]: {greeting_message}\n")
-    log.chat_logger.info(f"{user_name}, {user_name}")
+    response = bot.get_response("msgwme")
 
-    polite_greeting = "Hola, {name}. ¿Puedo ayudarte?".format(name=user_name)
-    print(f"[Crux] {polite_greeting}")
-    log.chat_logger.info(polite_greeting)
+    username = input(f"[{bot.name}]: {response}\n")
 
-    return user_name
+    return username
 
 
 def bot_showing_posts(api  # type: Api
@@ -185,23 +165,13 @@ def bot_showing_posts(api  # type: Api
 
     posts_info_list = list()
 
-    try:
-        log.debug_logger.debug("From pyfacebook_actions module, processor")
 
-    except:
-        print("Something went wrong with Facebook connection. Check your internet")
-        log.error_logger.error("From pyfacebook_actions module, processor failed")
+    posts_info_list = fb.get_posts(
+        api=api,
+        page_id=PAGE_ID
+    )
 
-    else:
-        log.debug_logger.debug("From data_management module, posts_reader")
-        posts_info_list = fb.get_posts(
-            api=api,
-            page_id=PAGE_ID
-        )
-        log.debug_logger.debug("From data_management module, posts_printing")
-        posts_printing(posts_info_list)
-
-        return posts_info_list
+    posts_printing(posts_info_list)
 
     return posts_info_list
 
@@ -222,25 +192,31 @@ def bot_liking_posts(api,  # type: Api
 
     else:
         bot_input_message = "Ingrese el numero de la publicacion que quiere likear:"
-        log.chat_logger.info(f"Crux, {bot_input_message}")
+
         post_number = input(f"[Crux]: {bot_input_message}\n")
-        log.chat_logger.info(f"{user_name}, {post_number}")
+
 
         while not post_number.isdecimal():
-            log.chat_logger.info(f"Crux, {bot_input_message}")
+
             post_number = input(f"[Crux]: {bot_input_message}\n")
-            log.chat_logger.info(f"{user_name}, {post_number}")
+
 
         post_id = posts_order(posts_info_list, post_number)
 
-    try:
         fb.post_like(api, post_id)
-
-    except:
-        print("Try rebooting your connection")
-        return False
+        
 
     return True
+
+
+def search_user_by_bot(api,  # type: IgProApi
+                       username  # type: str
+                       ):                        
+
+    data = ig.get_ig_user_info(
+        api = api,
+        username = username
+    )
 
 
 # ------------------------------------------------------ #
@@ -248,65 +224,77 @@ def bot_liking_posts(api,  # type: Api
 # ------------------------------------------------------ #
 
 
-def display_menu_fb():
-    print("1.Dar like a un posteo\n2.Leer posteo\n3.Subir posteo o foto\n4.Actualizar perfil\n5.Listar amigos\n6.Listar seguidores\n7.Seguir a un usuario\n8.Enviar un mensaje a un usuario")
+def set_up_username(username # type: str
+                    ):
 
-    
-def display_menu_ig():
-    print("1.Buscar usuario\n2.Listar seguidores\n3.Modificar publicacion\n4.Publicar foto\n5.Mostrar posteo")
+    # Read in the file
+    with open("data/entrenador.txt", "r", encoding = "utf-8") as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("{nombre}", username)
+
+    # Write the file out again
+    with open('data/entrenador.txt', 'w', encoding = "utf-8") as file:
+        file.write(filedata)
 
 
 def main():
+    response = ""
+    request = ""
+
     api = Api(
         app_id=APP_ID,
         app_secret=APP_SECRET,
         long_term_token=PAGE_ACCESS_TOKEN,
     )
 
-    log.debug_logger.debug("The program has just started...")
-    log.debug_logger.debug("bot_creation function has been executed")
-
     bot = bot_creation()
 
-    log.debug_logger.debug("bot_training function has been executed")
     bot_training(bot)
 
-    log.debug_logger.debug("bot_greetings function has been executed")
-    user_name = bot_greetings()
+    username = bot_greetings(bot)
 
-    finish = False
-    while not finish:
-        motivation = "Di algo..."
-        log.chat_logger.info(f"Crux, {motivation}")
-        user_message = input(f"[Crux]: {motivation}\n")
-        log.chat_logger.info(f"{user_name}, {user_message}")
+    set_up_username(username)
 
-        if user_message == "salir":
-            finish = True
-        else:
-            response = bot.get_response(user_message)
-            bot_name = bot.name
+    bot_training(bot)
 
-            if "Juan" in response.text:
-                print(f"[{bot_name}]: {response.text}")
-                log.chat_logger.info(f"Crux, {response.text}")
+    response = bot.get_response("Descripcion")
+    request = input(f"[{bot.name}]: {response}\n")
 
-                if "publicaciones" in response.text:
-                    log.debug_logger.debug("bot_showing_posts function has been executed")
-                    bot_showing_posts(api)
+    while request not in ["Salir", "salir", "Exit", "exit"]:
 
-                elif "likear" in response.in_response_to:
-                    log.debug_logger.debug("bot_liking_posts function has been executed")
-                    liked = bot_liking_posts(api, user_name)
+        if "bienvenida" in request:
+            response = bot.get_response(request)
+            print(f"[{bot.name}]: {response}\n")            
 
-                    if liked is True:
-                        success_message = "Post likeado con exito!"
-                        print(f"[Crux]: {success_message}\n")
-                        log.chat_logger.info(f"Crux, {success_message}")
+        if "opciones" in request:
+            response = bot.get_response(request)
+            request = input(f"[{bot.name}]: {response}\n")
+
+            if request in ["Facebook", "facebook", "fb"]:
+                for x in range(7):
+                    response = bot.get_response(f"fbopt{x}")
+                    print(f"[{bot.name}]: {response}")
+
+                request = input("\nEscribe la opción: ")
+
+            elif request in ["Instagram", "instagram", "ig"]:
+                for x in range(5):
+                    response = bot.get_response(f"igopt{x}")
+                    print(f"[{bot.name}]: {response}")
+
+                request = input("\nEscribe la opción: ")
 
             else:
-                print(f"[{bot_name}]: {response.text}")
-                log.chat_logger.info(f"Crux, {response.text}")
+                print("Ingresó una opción no valida")
+
+        response = bot.get_response("Continuar")
+        request = input(f"[{bot.name}]: {response}\n")        
+
+    else:
+        response = bot.get_response(request)
+        print(f"[{bot.name}]: {response}\n")
 
 
 if __name__ == "__main__":
